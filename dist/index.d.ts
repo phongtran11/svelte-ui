314ec74f627998c2d1326e63bc414b010da2cbbb
phongnarin <phongtran11.tt@gmail.com>
2024-11-05 21:03:10 +0700

    feat: export component

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,10 @@
-export {};
+import * as Accordion from './components/ui/accordion/index.js';
+import * as Avatar from './components/ui/avatar/index.js';
+import * as Button from './components/ui/button/index.js';
+import * as Calendar from './components/ui/calendar/index.js';
+import * as Checkbox from './components/ui/checkbox/index.js';
+import * as Card from './components/ui/card/index.js';
+import * as Form from './components/ui/form/index.js';
+import * as Label from './components/ui/label/index.js';
+import * as Dialog from './components/ui/dialog/index.js';
+export { Accordion, Avatar, Button, Calendar, Checkbox, Card, Form, Label, Dialog };
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,11 @@
-// Reexport your entry components here
+import * as Accordion from '$lib/components/ui/accordion/index.js';
+import * as Avatar from '$lib/components/ui/avatar/index.js';
+import * as Button from '$lib/components/ui/button/index.js';
+import * as Calendar from '$lib/components/ui/calendar/index.js';
+import * as Checkbox from '$lib/components/ui/checkbox/index.js';
+import * as Card from '$lib/components/ui/card/index.js';
+import * as Form from '$lib/components/ui/form/index.js';
+import * as Label from '$lib/components/ui/label/index.js';
+import * as Dialog from '$lib/components/ui/dialog/index.js';
+
+export { Accordion, Avatar, Button, Calendar, Checkbox, Card, Form, Label, Dialog };
